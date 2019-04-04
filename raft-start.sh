fcaffbe74117a59fa4df993d29c2413ff77273cd
zzy96 <zhou0250@e.ntu.edu.sg>
2019-04-04 17:14:20 +0800

    add raft api

--- raft-start.sh
+++ raft-start.sh
@@ -42,7 +42,7 @@ start_raft(){
         ARGS+=" --permissioned "
     fi
 
-    ARGS+=" --verbosity 5 --networkid $NETWORK_ID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --rpccorsdomain=* --rpcvhosts=* --ws --wsaddr 0.0.0.0 --wsorigins=* --wsapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --emitcheckpoints "
+    ARGS+=" --verbosity 5 --networkid $NETWORK_ID --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --rpccorsdomain=* --rpcvhosts=* --ws --wsaddr 0.0.0.0 --wsorigins=* --wsapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum,raft --emitcheckpoints "
 
     if [ "$peerid" != "0" ]
     then
