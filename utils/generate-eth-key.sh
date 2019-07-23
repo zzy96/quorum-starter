cf86cddc290fb512f13f171dedbb7e883d85fd61
zzy96 <zhou0250@e.ntu.edu.sg>
2019-07-23 20:10:44 +0800

    rename dev/contracts to dev/scripts, add utils

---
+++ utils/generate-eth-key.sh
@@ -0,0 +1,7 @@
+# `geth account` COMMANDS:
+#      list    Print summary of existing accounts
+#      new     Create a new account
+#      update  Update an existing account
+#      import  Import a private key into a new account
+
+geth account new --datadir "." --password "/dev/null"
---
+++ utils/generate-tessera-key.sh
@@ -0,0 +1,2 @@
+# tessera -keygen -filename /path/to/key1,/path/to/key2
+java -jar ${TESSERA_JAR} -keygen < /dev/null -filename tm
